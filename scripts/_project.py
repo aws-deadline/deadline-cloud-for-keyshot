d9fd17ffabf399f2464d9fa6bfe12860c547f834
Evan Spearman <evans@amazon.com>
2025-01-24 22:30:58 +0000

    fix: Removed endoding specification when opening pyproject.toml
    Signed-off-by: Evan Spearman <evans@amazon.com>

--- scripts/_project.py
+++ scripts/_project.py
@@ -25,7 +25,7 @@ def get_project_dict(project_path: Optional[Path] = None) -> dict[str, Any]:
         mode = "rb"
 
     with open(
-        str((project_path or get_git_root()) / "pyproject.toml"), mode, encoding="utf-8"
+        str((project_path or get_git_root()) / "pyproject.toml"), mode
     ) as pyproject_toml:
         return toml.load(pyproject_toml)
 
